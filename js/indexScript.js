a3d363c5cc567570a69c839a1e94d935eaaac651
Djeufo <chanelboris@gmail.com>
2025-03-07 09:17:06 +0100

    added registration form

--- js/indexScript.js
+++ js/indexScript.js
@@ -1,10 +1,13 @@
-
-// Get the modal
-var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+// Function to close the modal when clicking outside
+function closeModalOnClick(modalId) {
+    var modal = document.getElementById(modalId);
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
     }
 }
+
+// Apply the function to each modal
+closeModalOnClick('id01');
+closeModalOnClick('id02');
