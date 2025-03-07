4fe82075a89f5d73ae6dc1eaa101d126950241c4
Djeufo <chanelboris@gmail.com>
2025-03-07 10:54:56 +0100

    fix javascript , when click outside the model , make it use evenlistener so that it should not ovveride the previous event

--- js/indexScript.js
+++ js/indexScript.js
@@ -1,11 +1,12 @@
 // Function to close the modal when clicking outside
 function closeModalOnClick(modalId) {
     var modal = document.getElementById(modalId);
-    window.onclick = function(event) {
-        if (event.target == modal) {
+    modal.addEventListener('click', function(event) {
+        // Check if the click was outside the modal content area
+        if (event.target === modal) {
             modal.style.display = "none";
         }
-    }
+    });
 }
 
 // Apply the function to each modal
